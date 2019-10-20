4522cfb87fad063a6afcd88e525c5b7b2c0aa755
qig123 <qiuguoxyz2@gmail.com>
2019-10-20 11:27:06 +0800

    sicp init

---
+++ abs_pro.rkt
@@ -0,0 +1,27 @@
+#lang sicp
+
+(define (deriv g)
+  (lambda (x)
+    ( / ( -( g (+ x dx))(g x)) dx)
+    ))
+(define dx 0.00001)
+(define (cube x) (* x x x))
+
+(define (newton-transform g)
+  ( lambda (x)
+     ( - x (/ (g x) ( (deriv g) x)  ) )
+
+  ))
+(define tolerance 0.00001)
+(define (fixed-point f first-guess)
+  (define (close-enough? v1 v2) (< (abs (- v1 v2)) tolerance))
+  (define (try guess)
+   ( let (next (f guess))
+      (if (close-enough? guess next) next  (try next))
+      ))
+  (try first-guess )
+  )
+
+
+
+  
