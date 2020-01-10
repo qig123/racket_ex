c464843780654c75660470f5bd876bfcda3ffbf6
qig123 <qiuguoxyz2@gmail.com>
2020-01-10 15:42:51 +0800

    22习题

--- 2/21/22.rkt
+++ 2/21/22.rkt
@@ -1,10 +1,17 @@
 #lang racket
 ; midpoint
+;ex (print-point (midpoint-segment (make-segment (make-point 0 0 ) (make-point 1 1 ))))
+
+
 (define (midpoint-segment s)
    (make-point ( mid  (x-point (start-segment s) ) (x-point (end-segment s)) )
                ( mid  (y-point (start-segment s) ) (y-point (end-segment s)))
                )
   )
+(define (distance-segment x y)
+   (sqrt ( + (sq ( - (x-point x) (x-point y) )) (sq ( - (y-point x) (y-point y) ) )))
+  )
+(define (sq x) (* x x))
 
 (define (make-segment s1 s2)
   (cons s1 s2)
@@ -25,6 +32,18 @@
 (define (make-rectangle w h) (cons w h))
 (define (w-rectangle r) (car r))
 (define (h-rectangle r) (cdr r))
+
+
+(define (r-circumference r)
+   (+ (* 2 (distance-segment   (start-segment (w-rectangle r))   (end-segment (w-rectangle r))  ) )
+      (* 2 (distance-segment   (start-segment (h-rectangle r))   (end-segment (h-rectangle r))  ))
+      )
+  )
+(define (r-area r)
+   (* (w-rectangle r) (h-rectangle r) )
+  )
+
+
 
 (define (mid x y) (/ (+ x y) 2))
 
---
+++ 2/21/300.rkt
@@ -0,0 +1,10 @@
+#lang racket
+(define (make-withdraw balance)
+  (lambda (amount)
+    (if (>= balance amount)
+        (begin (set! balance (- balance amount)) balance)
+        ("Insufficient funds")
+        )
+    )
+  )
+(define w1 (make-withdraw 100))
