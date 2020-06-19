c71eea9622d422b013337d3c9e3aaf5fa2b6b93a
guo_qiu <qiuguoxyz2@gmail.com>
2020-06-19 17:14:33 +0800

    ex_5052练习

--- char3/char3_ex50_52.rkt
+++ char3/char3_ex50_52.rkt
@@ -1,16 +1,4 @@
 #lang Racket
-(provide print-cake)
-; draws a cake with n candles
-(define (print-cake n)
-  (show "   ~a   " n #\.)
-  (show " .-~a-. " n #\|)
-  (show " | ~a | " n #\space)
-  (show "---~a---" n #\-))
- 
-(define (show fmt n ch)
-  (printf fmt (make-string n ch))
-  (newline))
-
 
 
 
--- char3/cons_stream_define.rkt
+++ char3/cons_stream_define.rkt
@@ -1,10 +1,15 @@
 #lang Racket
+(provide cons-stream)
+(provide stream-car)
+(provide stream-cdr)
+;把计算的行为保存为数据
 (define (memo-proc proc)
   (let ((already-run? false) (result false))
     (lambda ()
       (if (not already-run?)
           (begin (set! result (proc))
                  (set! already-run? true)
+                 result
                  )
           result
           )
@@ -17,23 +22,96 @@
 (define (force delayed-object)
   (delayed-object))
 (define (sqr x) (* x x))
-(define (test t)
-  (lambda (t) (* t 2))
-  )
+
 (define (cons-stream a b)
              (cons a (delay b))
              )
-( define (stream-car x) (car x))
-(define (stream-cdr x) (cdr x))
+(define (stream-car x) (car x))
+(define (stream-cdr x) (force (cdr x)))
 
 (define (stream-null? stream) (null? stream))
 (define (stream-filter pred stream)
   (cond
     ((stream-null? stream) null)
-    ((pred (stream-car)) (cons-stream (stream-car stream)
-                                      (stream-filter pred (stream-cdr stream))
-                                      ))
-    (else (stream-filter pred (stream-cdr stream)))
+    ((pred (stream-car stream))
+     (cons-stream (stream-car stream)
+                  (stream-filter pred (stream-cdr stream))
+                  )
+     )
+    (else
+     (stream-filter pred (stream-cdr stream))
+     )
     )
   )
+
+(define (stream-enumerate-interval low high)
+  (if (> low high)
+      null
+      (cons-stream
+       low
+       (stream-enumerate-interval (+ low 1) high)
+       )
+      )
+  )
+;ref操作
+(define (stream-ref s n)
+  (if (= n 0)
+      (stream-car s)
+      (stream-ref (stream-cdr s) (- n 1))
+      )
+  )
+;(stream-ref one-to-ten 2)
+(define (stream-for-each proc s)
+  (if(stream-null? s)
+     'done
+     (begin (proc (stream-car s))
+            (stream-for-each proc (stream-cdr s)))
+      )
+  )
+(define (display-line x)
+  (newline)
+  (display x)
+  )
+(define (display-stream s)
+  (stream-for-each display-line s)
+  )
+
+
+(define one-to-ten (stream-enumerate-interval 1 10))
+(define one-to-two (stream-enumerate-interval 1 2))
+;利用map内部实现的可变参数功能
+(define (stream-map proc . list-of-stream)
+  (if (null? (car list-of-stream))
+      null
+      (cons-stream
+       (apply proc (map stream-car list-of-stream))
+       (apply stream-map
+              (cons proc (map stream-cdr  list-of-stream))
+              )
+       )
+      )
+  )
+;
+(define (show x)
+  (display-line x)
+  x
+  )
+;(define x (stream-map show (stream-enumerate-interval 0 10)))
+(define sum 0)
+(define (accum x)
+  (set! sum (+ x sum))
+  sum
+  )
+;(define seq (stream-map accum (stream-enumerate-interval 1 20)))
+;(define y (stream-filter even? seq))
+;(define z (stream-filter (lambda (x) (= (remainder x 5) 0)) seq))
+;(stream-ref y 7)
+;(display-stream z)
+
+
+
+
+
+
+
 
